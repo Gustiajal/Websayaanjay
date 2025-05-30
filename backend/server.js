d6a867877bb5735070964041be27c9a9fde35ca2
Arif Rachman Ramadhon <arifr0698@gmail.com>
2025-05-31 00:14:23 +0700

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express()
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.get('/', (req,res) => {
+    res.json({message:"Hello from the server"});
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
